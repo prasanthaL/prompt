abf07401d5df26657246dbe4a8afbac97bbbf4fe
TrivexMalith <malith.trivextech@gmail.com>
2026-06-28 17:36:28 +0530

    pinterest linking added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -448,7 +448,7 @@ export default async function Home({ searchParams }: PageProps) {
                   </div>
                   <div className="flex items-center gap-1">
                     <Clock className="w-3 h-3" />
-                    5 min read
+                    6 min read
                   </div>
                 </div>
               </div>
